02f1da371e6e626aa8a1c3ec9a8274b5c6b5bc2f
Alexandr Bochev <abochev@gmail.com>
2022-05-19 11:10:58 +0300

    Header logo & switch

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
-const test = (hello) => {
-  hello = 'Hello'
-  return hello
+// Header logo & switch
+var btn = document.getElementById('btn')
+var toggleBtnHome = document.getElementById('toggleBtnHome')
+var toggleBtnOffice = document.getElementById('toggleBtnOffice')
+
+function leftClick() {
+  btn.style.left = '2px'
+  btn.style.width = '55px'
+  toggleBtnHome.style.color = '#fff'
+  toggleBtnOffice.style.color = '#636C81'
 }
-console.log(test())
+
+function rightClick() {
+  btn.style.left = '61px'
+  btn.style.width = '66px'
+  toggleBtnHome.style.color = '#636C81'
+  toggleBtnOffice.style.color = '#fff'
+}
+
